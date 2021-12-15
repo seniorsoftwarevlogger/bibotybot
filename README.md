89117ac66f5d7536ac87687699ce3e8ccfab38bb
Dmitriy (Dima) Rozhkov <rojkov.dmitry+github@gmail.com>
2021-12-15 22:38:28 +0100

    feat: t.me banning bot

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-# Send YouTube Comments to Telegram
+# Cut the BS on Telegram 
 
-FOMO is not allowed. Configure the bot to check your YouTube channel periodically and send all the new comments your way. No need to be checking comments compulsively. No need to enable YouTube's native notification.
-
-Be in control.
-
-## Setup
-
-It stores data in Sqlite3 database and will not send the messages twice.
-
-### Deliver messages
-
-Add something like this to the `cron``
-
-```
-0 7 * * * NODE_ENV=production BOT_TOKEN= TELEGRAM_CHAT_ID= GOOGLE_API_KEY= YOUTUBE_CHANNEL_ID= node scripts/check-messages.js
-```
+Simple bot that deletes t.me links from the channel posts.
 
 ### Start the bot
 
@@ -32,9 +18,6 @@ http://<domain>:<PORT>/<BOT_TOKEN>
 
 ```
 BOT_TOKEN=
-TELEGRAM_CHAT_ID=
-GOOGLE_API_KEY=
-YOUTUBE_CHANNEL_ID=
 ```
 
 ### Optional ENV variables
@@ -46,12 +29,3 @@ SENTRY_DSN=
 ## Features
 
 1. **i18n**: русский, english
-
-### Roadmap
-
-1. Reply to comments repying to Telegram messages
-2. Treshold. Don't notify about emoji comments
-3. Stop list. Don't notify about certain people's messages.
-4. Load more comments if the first page is all new.
-5. Filters: subscribers / members
-6. Track replies
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Telegraf } = require("telegraf");
 const i18n = require("i18n");
-const sqlite3 = require("sqlite3");
 const Sentry = require("@sentry/node");
 
 // Setup =======================================================================
@@ -30,8 +29,6 @@ if (missingEnv.length > 0) {
   process.exit(1);
 }
 
-const db = new sqlite3.Database(__dirname + "/database");
-
 // Main ========================================================================
 
 const bot = new Telegraf(BOT_TOKEN, {
@@ -40,44 +37,16 @@ const bot = new Telegraf(BOT_TOKEN, {
   },
 });
 
-bot.on("text", (ctx) => {
-  const matches = ctx.message.text.match(/добавь (.*)/);
-  if (matches && matches[1]) {
-    db.run(
-      "INSERT INTO youtube_channels (channel_id, telegram_chat_id) VALUES (?, ?)",
-      [matches[1], ctx.chat.id],
-      (err) => {
-        if (!err) return ctx.reply("Канал добавлен!");
-        else ctx.reply("Что-то пошло не так :(");
-      }
-    );
-  } else if (ctx.message.text === "каналы") {
-    db.all(
-      "SELECT * FROM youtube_channels WHERE telegram_chat_id = ?",
-      [ctx.chat.id],
-      (err, rows) => {
-        if (err) return ctx.reply("Канал добавлен!");
-
-        ctx.replyWithMarkdown(
-          `Каналы подключенные к этому чату:
-${rows.map((row) => `- ${row["channel_id"]}`).join("\n")}`,
-          { disable_web_page_preview: true }
-        );
-      }
-    );
-  } else {
-    ctx.replyWithMarkdown(`Я не понял команду. Попробуй:
-    - добавь [адрес канала]
-    - каналы
-    `);
-  }
+bot.hears(/t\.me\//, (ctx, _post) => {
+  console.log(`DELETING: ${ctx.message.message_id} ${ctx.message.text}`);
+  ctx.deleteMessage(ctx.message.message_id);
 });
 
 const botOptions =
   NODE_ENV === "production"
     ? {
         webhook: {
-          hookPath: BOT_TOKEN,
+          domain: WEBHOOK_URL,
           port: parseInt(PORT, 10),
         },
       }
@@ -85,5 +54,8 @@ const botOptions =
         polling: { timeout: 30, limit: 10 },
       };
 
-bot.telegram.setWebhook(WEBHOOK_URL);
 bot.launch(botOptions);
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
