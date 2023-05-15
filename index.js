1035e6647027ddec3b37b23ff528032129171517
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2023-05-15 22:59:44 +0200

    feat: allow list and media from family

--- index.js
+++ index.js
@@ -21,9 +21,14 @@ if (process.env.SENTRY_DSN) {
   Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
 
-const missingEnv = ["ME", "PORT", "BOT_TOKEN", "WEBHOOK_URL"].filter(
-  (e) => !process.env[e]
-);
+const missingEnv = [
+  "ME",
+  "PORT",
+  "BOT_TOKEN",
+  "WEBHOOK_URL",
+  "ALLOW",
+  "FAMILY",
+].filter((e) => !process.env[e]);
 
 const { ME, PORT, BOT_TOKEN, NODE_ENV, WEBHOOK_URL } = process.env;
 
@@ -42,32 +47,65 @@ const bot = new Telegraf(BOT_TOKEN, {
 
 const deletedMessages = [];
 const myChannels = ME.split(",");
+const allowList = ALLOW.split(",");
+const family = FAMILY.split(",");
 
-function isMe({message}) {
-  return message.from.first_name === "Telegram" || (message.from.first_name === "Channel" && myChannels.includes(message.sender_chat?.username));
+function isMe({ message }) {
+  return (
+    message.from.first_name === "Telegram" ||
+    (message.from.first_name === "Channel" &&
+      myChannels.includes(message.sender_chat?.username))
+  );
 }
-function isChannelBot({message}) {
+function isAllowList({ message }) {
+  return allowList.includes(message.sender_chat?.username);
+}
+function isChannelBot({ message }) {
   return message.from.first_name === "Channel";
 }
 function hasTelegramLink(ctx) {
-  return ctx.message.text?.includes("t.me")
+  return ctx.message.text?.includes("t.me");
 }
 
 const spamChecks = [isChannelBot, hasTelegramLink];
 
-bot.on('message', (ctx) => {
+bot.on("message", (ctx) => {
   console.log(ctx.message.from);
-  
-  if(isMe(ctx)) return;
+
+  // test if message is not text and not from family
+  if (!ctx.message.text && !family.includes(ctx.message.from.username)) {
+    // block user from sending media
+    ctx.restrictChatMember(ctx.message.from.id, {
+      permissions: {
+        can_send_messages: true,
+        can_send_media_messages: false,
+        can_send_polls: false,
+        can_send_other_messages: false,
+        can_add_web_page_previews: false,
+        can_change_info: false,
+        can_invite_users: false,
+        can_pin_messages: false,
+      },
+    });
+
+    ctx
+      .deleteMessage(ctx.message.message_id)
+      .then(() => deletedMessages.push(ctx.message.message_id))
+      .catch((e) => console.log("CANT DELETE:", ctx.message, e));
+  }
+
+  // test link spam
+  if (isMe(ctx) || isAllowList(ctx)) return;
 
   if (spamChecks.some((check) => check(ctx))) {
     console.log(`DELETING: ${ctx.message.message_id} ${ctx.message.text}`);
 
-    ctx.deleteMessage(ctx.message.message_id)
+    ctx
+      .deleteMessage(ctx.message.message_id)
       .then(() => deletedMessages.push(ctx.message.message_id))
       .catch((e) => console.log("CANT DELETE:", ctx.message, e));
   }
-})
+});
 
 // TODO: replace name handle with chat id
 // setInterval(() => {
@@ -75,20 +113,19 @@ bot.on('message', (ctx) => {
 //    .catch((e) => console.log("CANT SEND MESSAGE:", e));
 // }, 1000 * 60 * 60 * 24);
 
-const botOptions =
-  isProduction
-    ? {
-        webhook: {
-          domain: WEBHOOK_URL,
-          port: parseInt(PORT, 10),
-        },
-      }
-    : {
-        polling: { timeout: 30, limit: 10 },
-      };
+const botOptions = isProduction
+  ? {
+      webhook: {
+        domain: WEBHOOK_URL,
+        port: parseInt(PORT, 10),
+      },
+    }
+  : {
+      polling: { timeout: 30, limit: 10 },
+    };
 
 bot.launch(botOptions);
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
